81b94c09bb864062da01ecef86dd0313e006ad05
manvi0012 <manvi.barlota2010@gmail.com>
2024-12-29 18:38:19 +0530

    commiting my changes

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-// alert("Hello World")
-// step 1 download 6 images
-// name them properly [dice-1.jpeg]
-// add a img tag
-
-
 function buttonClicked(){
     const numbers = [1,2,3,4,5,6];
     const outcome = numbers[Math.floor(Math.random() * numbers.length)];
@@ -17,9 +10,27 @@ function numButton(digit){
 
 function clearButton(){
     document.getElementById('hit').innerHTML = ''
+    document.getElementById('ans').innerHTML = ''
 }
+
 function backButton(){
     let hitContent = document.getElementById('hit').innerHTML;
     document.getElementById('hit').innerHTML = hitContent.slice(0, -1);
 }
 
+function opearatorButton(op){
+    previous(document.getElementById('hit').innerHTML.slice(-1))
+    document.getElementById('hit').innerHTML += op
+}
+
+function equalToButton() {
+    previous(document.getElementById('hit').innerHTML.slice(-1))
+    let content = document.getElementById('hit').innerHTML;
+    let result = eval(content);
+    document.getElementById('ans').innerHTML = result
+}
+
+function previous(content){
+    if(content == '/' || content == '+' || content == '-' || content == '*' || content ==  '.' )
+        backButton()
+}
